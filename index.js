64ac981c079105343c6f4892a1ec8c50a2ff61cb
diogo <diogo.lima.flooxai@gmail.com>
2025-03-17 08:13:48 -0300

    feat:implementing variables

---
+++ index.js
@@ -0,0 +1,46 @@
+// Declarando variavéis
+
+// operador de atribuição (=)
+var olaMundo = "Hello World!"
+
+console.log("olá mundo")
+
+// operador de comparação (==)
+let a = 10 ;
+const b = 20 ;
+console.log(a == b);
+
+// compara o valor e o tipo de dados (===)
+
+let c = 10
+const d = "20"
+console.log(c === d);
+
+// compara se os valores e os tipos são diferentes (!==)
+
+let e = 10
+const f = "20"
+console.log(e !== f); 
+
+// && end and OR
+let g = 10
+const h = "20"
+console.log(g == h || typeof g == 'string'); 
+
+// if and Else
+
+let cor = 'yellow';
+
+if(cor === 'black') {
+
+  console.log('follow')
+
+} else if (cor === "yellow") {
+
+  console.log('attention')
+
+} else {
+
+  console.log('Stop')
+
+}
