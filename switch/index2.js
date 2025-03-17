c3742355a1eb9a1b35f388fa4cffb3c784c8385a
diogo <diogo.lima.flooxai@gmail.com>
2025-03-17 08:28:57 -0300

    feat: implementing the switch

---
+++ switch/index2.js
@@ -0,0 +1,18 @@
+let cor = "blue";
+
+switch (cor) {
+  case "green":
+    console.log("follow");
+    break;
+    
+  case "yellow":
+    console.log("attention");
+    break;
+
+  case "red":
+    console.log("stop");
+    break;
+
+    default:
+      console.log('I don t know')
+}
