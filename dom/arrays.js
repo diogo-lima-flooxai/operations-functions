438de1336696b6179c86d18192a6ee8b8c2df6e5
diogo <diogo.lima.flooxai@gmail.com>
2025-03-17 09:35:39 -0300

    feat: implementing DOM, EVENTS, ARRAYS, forEach and DATA actions

---
+++ dom/arrays.js
@@ -0,0 +1,5 @@
+let carros = ["palio 98", "toro", 'uno', 10, true, new Date(), function(){}]
+
+carros.forEach(function(value, index){
+  console.log(index, value);
+})
---
+++ dom/forEach.js
@@ -0,0 +1,3 @@
+let carros = ["palio 98", "toro", 'uno', 10, true, new Date(), function(){}]
+
+console.log(carros[5].getFullYear())
